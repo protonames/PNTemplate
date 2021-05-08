2f33cec8467e297949904c00d684222d9748e315
Felipe Nunes <felipe@protonam.es>
2021-05-08 01:58:36 +0100

    Add SuperUnityBuild

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,37 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Release,
+    }
+
+    public enum Platform
+    {
+        None,
+        WebGL,
+        PC,
+    }
+
+    public enum Architecture
+    {
+        None,
+        WebGL,
+        Windows_x86,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637560353243681637);
+    public const string version = "1.1";
+    public const ReleaseType releaseType = ReleaseType.Release;
+    public const Platform platform = Platform.PC;
+    public const Architecture architecture = Architecture.Windows_x86;
+    public const Distribution distribution = Distribution.None;
+}
+
